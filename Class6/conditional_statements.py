38ae3256293ad2f3fceec0ea15c4994cce1d5c83
Bisma Shafiq <148833585+Bisma-Shafiq@users.noreply.github.com>
2024-08-26 11:44:07 +0500

    Class6_Assignment(Basic Python)

---
+++ Class6/conditional_statements.py
@@ -0,0 +1,8 @@
+number = int(input("enter a number"))
+
+if number>=100:
+    print("passed")
+else:
+    print("fail")
+
+    
---
+++ Class6/tuple.py
@@ -0,0 +1,4 @@
+number = (1,2,3,4,5,8)
+print(number)
+print(type(number))
+
