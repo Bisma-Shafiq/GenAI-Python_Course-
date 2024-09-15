47ebb7886536f52dbd4d2bcc89a30b739eb2ea64
Bisma Shafiq <bismashafiq26@gmail.com>
2024-09-14 19:02:18 -0700

    Dictionary file.py

--- Class7/dict.py
+++ Class7/dict.py
@@ -1,2 +1,4 @@
-dict= {"name": "bisma" , "grade": "A+" }
-print(dict)
+fruits: dict = {'A':'apple', 'B':'Banana', 'P':'pear', 'M':'mango'}
+print(fruits)
+#find value of a given key
+print(fruits['A'])
